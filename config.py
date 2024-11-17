292c21cb0e695239b32f6dffd025218f1e441681
Ahimenes17 <prokopetsnastya1702@gmail.com>
2024-11-17 12:28:46 +1000

    add data

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 token = "Свой токен"
 folder_id = "Свой идентификатор каталога"
 text = "Текст для приветствия"
+
+dataForYandexSpeechKit = {
+    'text': text,
+    'lang': 'ru-RU',
+    'voice': 'marina',
+    'emotion': 'friendly',
+    'format': 'lpcm',
+    'sampleRateHertz': '8000'
+}
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from config import *
+import os
+import argparse
+from asterisk.agi import *
+import requests
+import grpc
+import yandex.cloud.ai.stt.v2.stt_service_pb2 as stt_service_pb2
+import yandex.cloud.ai.stt.v2.stt_service_pb2_grpc as stt_service_pb2_grpc
 
+agi = AGI()
+CHUNK_SIZE = 4000
 folder = folder_id
 iam_token = token
-textForSpeech = text
-
+dataForSpeech = data
 
